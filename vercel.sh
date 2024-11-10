b2637c5e665670ed28f6228111c6ae69a7287e11
my_eell <73692114+eell-os@users.noreply.github.com>
2024-11-10 18:11:39 +0800

    vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 下载 dist.tar.gz 文件
-wget https://github.com/louislam/uptime-kuma/releases/download/1.23.15/dist.tar.gz
+curl -L https://github.com/louislam/uptime-kuma/releases/download/1.23.15/dist.tar.gz
 
 # 解压文件
 tar -xzf dist.tar.gz
